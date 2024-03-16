6d844c514c602839640b52ea088a8b4d3d1ecd9b
Ilikepizza2006 <146470829+Ilikepizza2006@users.noreply.github.com>
2024-03-16 19:35:39 +0100

    1.0

---
+++ mods/pizzascountries.js
@@ -0,0 +1,9 @@
+nations.MS = {
+    key: "MS", /*key, make sure its the same as the tag, frances tag is FRA so for managing france u will do nations.FRA , key value has to be the same as the tag */
+    name: "The Moss Republic", /*Name of the nation*/
+    power: 25, /*Starting power */
+    changeMin: 1, /*this is the ideal, higher it is = less power */
+    changeMax: 5, /* higher it is = more power, 5 is for examply Germany's */
+    mood: "Hungry", /*Set this to anything, it wont matter*/
+    color: ["#355E3B", "#114232"]
+}
