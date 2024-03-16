c482cb5d4ba6fa98db9247e9f53171e565efe45c
Jayd-Rubies <155784127+Jayd-Rubies@users.noreply.github.com>
2024-03-16 14:27:55 -0400

    Add files via upload

--- mods/Maryland.js
+++ mods/Maryland.js
@@ -1,7 +1,7 @@
-nations.Maryland = {
+nations.MYL = {
     key: "MYL", /*key, make sure its the same as the tag, frances tag is FRA so for managing france u will do nations.FRA , key value has to be the same as the tag */
-    name: "Mayland", /*Name of the nation*/
-    color: ["#000000", "#ffff00"] /* make sure there are atleast 2 colors otherwise it wont work */
+    name: "Maryland", /*Name of the nation*/
+    color: ["#000000", "#ffff00"], /* make sure there are atleast 2 colors otherwise it wont work */
     power: 10000, /*Starting power */
     changeMin: 0.1, /*this is the ideal, higher it is = less power */
     changeMax: 10000, /* higher it is = more power, 5 is for examply Germany's */
