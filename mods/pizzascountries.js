e80e27ffea2ae964b66f6dfe6d25b1e7a62408de
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2024-03-16 21:27:16 +0200

    Update pizzascountries.js

--- mods/pizzascountries.js
+++ mods/pizzascountries.js
@@ -11,44 +11,12 @@ nations.MS = {
 canTick = false
 let modTick = true
 function customTick() {
-  console.log('works')
-  if (modTick == true) {
-    backMap = {}
-    for (const k in nations) {
-      nations[k].power += Math.floor(Math.random() * (nations[k].changeMax - nations[k].changeMin + 1) - nations[k].changeMin)
-      if (nations[k].power < 0 && nations[k].power > -50) {
-          nations[k].mood = "sad";
-      } else if (nations[k].power < -49) {
-          nations[k].mood = "pain";
-      } else if (nations[k].power > 0 && nations[k].power <= 50) {
-          nations[k].mood = "normal";
-      } else if (nations[k].power > 50 && nations[k].power <= 135) {
-          nations[k].mood = "stronk";
-      } else if (nations[k].power > 135) {
-          nations[k].mood = "super stronk";
-      }
-
-      if ("FRA" in nations && "GER" in nations) {
-        nations.GER.mood = "pain (france exists) "
-      } else if ('GER' in nations && 'RUS' in nations && 'POL' in nations) {
-          nations.POL.mood = "mega fear"
-      } else if (('RUS' in nations || 'GER' in nations)  && 'POL' in nations) {
-          nations.POL.mood = "fear"
-      }
-    }
-    const natsList = document.getElementById('nats');
-    while (natsList.firstChild) {
-      natsList.removeChild(natsList.firstChild);
-    }
-    for (const k in nations) {
-      const nl = document.createElement('li');
-      nl.onclick = function() {
-        bonus(bType, nations[k].key)
-      }
-      nl.textContent = k + ": " + nations[k].name + ", POWER: " + nations[k].power + ", MOOD: " + nations[k].mood;
-      nl.style = "background-image: linear-gradient(to right, " + nations[k].color + ");" + " background-size: 865px 30px;" + " background-repeat: no-repeat; cursor: pointer;"
-      natsList.appendChild(nl);
-    }
+  if ("FRA" in nations && "GER" in nations) {
+    nations.GER.mood = "pain (france exists) "
+  } else if ('GER' in nations && 'RUS' in nations && 'POL' in nations) {
+    nations.POL.mood = "mega fear"
+  } else if (('RUS' in nations || 'GER' in nations)  && 'POL' in nations) {
+    nations.POL.mood = "fear"
   }
 }
 setInterval(customTick, 500)
