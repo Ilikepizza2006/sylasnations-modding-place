b9b8e7e706ac1139bc5c3a4a3d19c29448a992a1
GachaYTB <pookiemon2019@gmail.com>
2024-06-22 18:18:47 +0200

    im 100% sure this fix will work

--- mods/WWIIIlauncher.js
+++ mods/WWIIIlauncher.js
@@ -1,17 +1,5 @@
 // a launcher for World War III.
 
-function launchWWIII() {
-    const entries = Object.entries(nations);
-    const fridx = 7;
-    const gridx = 0;
-    const [key1, value1] = entries[fridx];
-    one = { key: key1, ...value1 };
-    const [key2, value2] = entries[gridx];
-    two = { key: key2, ...value2 };
-    document.getElementById('1').textContent = one.name;
-    document.getElementById('2').textContent = two.name;
-}
-
 const wwiiilauncher = document.createElement("button");
 wwiiilauncher.id = "WWIIILauncher";
 wwiiilauncher.onclick = launchWWIII;
@@ -24,4 +12,4 @@ wwiiilauncher.style.backgroundSize = "50% 100% 50% 100%";
 wwiiilauncher.style.backgroundRepeat = "no-repeat";
 wwiiilauncher.style.borderRadius = "10px";
 const htmlofbtn = wwiiilauncher.outerHTML;
-document.getElementById('a').outerHTML = document.getElementById('a').outerHTML + "<button id='WWIIILauncher' style='position: absolute; margin-left: 15px; margin-top: 1px; background-image: linear-gradient(to right, rgb(97, 255, 129), rgb(255, 234, 0)); background-repeat: no-repeat; border-radius: 10px;'>Launch WWIII</button>"
+document.getElementById('a').outerHTML = document.getElementById('a').outerHTML + "<button id='WWIIILauncher' style='position: absolute; margin-left: 15px; margin-top: 1px; background-image: linear-gradient(to right, rgb(97, 255, 129), rgb(255, 234, 0)); background-repeat: no-repeat; border-radius: 10px;' onclick=\"const entries = Object.entries(nations);const fridx = 7;const gridx = 0;const [key1, value1] = entries[fridx];one = { key: key1, ...value1 };const [key2, value2] = entries[gridx];two = { key: key2, ...value2 };document.getElementById('1').textContent = one.name;document.getElementById('2').textContent = two.name;\">Launch WWIII</button>"
