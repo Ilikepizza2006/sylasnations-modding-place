bc3907c529a4f8545159272632f7e441f7b8ccf7
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2024-06-22 20:16:36 +0300

    Update sovietonion.js

--- mods/sovietonion.js
+++ mods/sovietonion.js
@@ -2,3 +2,4 @@ nations.RUS.name = 'Soviet Union'
 nations.RUS.changeMax += 1
 nations.RUS.attributes = ["noformat"]
 nations.RUS.ideology = 'communism'
+nations.RUS.color = ['#ffde00', '#ff0000', '#ff0000']
