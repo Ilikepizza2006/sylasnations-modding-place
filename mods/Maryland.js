48e2940c12f30a5a974b1b0208ba945db2ec4982
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2024-06-22 18:01:13 +0300

    Update Maryland.js

--- mods/Maryland.js
+++ mods/Maryland.js
@@ -1,6 +1,7 @@
 nations.MYL = {
     key: "MYL", /*key, make sure its the same as the tag, frances tag is FRA so for managing france u will do nations.FRA , key value has to be the same as the tag */
     name: "Maryland", /*Name of the nation*/
+    ideology: "democratic",
     color: ["#000000", "#ffff00"], /* make sure there are atleast 2 colors otherwise it wont work */
     power: 10000, /*Starting power */
     changeMin: 0.1, /*this is the ideal, higher it is = less power */
