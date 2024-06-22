3a993b007fbe16f6b3110749ee8c6877b0d5e439
DaviStudios <112717418+DaviStudios@users.noreply.github.com>
2024-06-22 20:33:16 +0300

    Update WWIIIlauncher.js

--- mods/WWIIIlauncher.js
+++ mods/WWIIIlauncher.js
@@ -23,4 +23,4 @@ wwiiilauncher.style.backgroundImage = "linear-gradient(to right, #61ff81, #ffea0
 wwiiilauncher.style.backgroundSize = "50% 100% 50% 100%";
 wwiiilauncher.style.backgroundRepeat = "no-repeat";
 wwiiilauncher.style.borderRadius = "10px";
-document.getElementById('a').appendChild(wwiiilauncher)
+document.getElementById('warbutton').appendChild(wwiiilauncher)
