20fc4154927a9bd6b6d78c6e52c4d9fe859c77a8
GachaYTB <pookiemon2019@gmail.com>
2024-06-23 09:31:51 +0200

    change france idx because of a country that got removed for some reason

--- mods/WWIIIlauncher.js
+++ mods/WWIIIlauncher.js
@@ -1,24 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const wwiiilauncher = document.createElement("button");
-    wwiiilauncher.id = "WWIIILauncher";
-    wwiiilauncher.innerText = "Launch WWIII";
-    wwiiilauncher.style.position = "relative";
-    wwiiilauncher.style.marginLeft = "15px";
-    wwiiilauncher.style.marginTop = "1px";
-    wwiiilauncher.style.backgroundImage = "linear-gradient(to right, #61ff81, #ffea00)";
-    wwiiilauncher.style.backgroundRepeat = "no-repeat";
-    wwiiilauncher.style.borderRadius = "10px";
-    wwiiilauncher.addEventListener('click', function() {
-        const entries = Object.entries(nations);
-        const fridx = 7;
-        const gridx = 0;
-        const [key1, value1] = entries[fridx];
-        one = { key: key1, ...value1 };
-        const [key2, value2] = entries[gridx];
-        two = { key: key2, ...value2 };
-        document.getElementById('1').textContent = one.name;
-        document.getElementById('2').textContent = two.name;
-    });
-    const container = document.getElementById('a');
-    container.appendChild(wwiiilauncher);
-});
+function launchWWIII() {
+    const entries = Object.entries(nations);
+    const fridx = 6;
+    const gridx = 0;
+    const [key1, value1] = entries[fridx];
+    one = { key: key1, ...value1 };
+    const [key2, value2] = entries[gridx];
+    two = { key: key2, ...value2 };
+    document.getElementById('1').textContent = one.name;
+    document.getElementById('2').textContent = two.name;
+}
+
+const wwiiilauncher = document.createElement("button");
+wwiiilauncher.id = "WWIIILauncher";
+wwiiilauncher.onclick = function() { //<------- !!!
+    launchWWIII()
+}; 
+wwiiilauncher.innerText = "Launch WWIII";
+wwiiilauncher.style.position = "absolute";
+wwiiilauncher.style.marginLeft = "15px";
+wwiiilauncher.style.marginTop = "1px";
+wwiiilauncher.style.backgroundImage = "linear-gradient(to right, #61ff81, #ffea00)";
+wwiiilauncher.style.backgroundSize = "50% 100% 50% 100%";
+wwiiilauncher.style.backgroundRepeat = "no-repeat";
+wwiiilauncher.style.borderRadius = "10px";
+document.getElementById('warbutton').appendChild(wwiiilauncher)
