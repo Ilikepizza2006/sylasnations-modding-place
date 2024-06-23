dab45119ee226ab0a0aec850875bd4437edac425
GachaYTB <pookiemon2019@gmail.com>
2024-06-23 09:56:54 +0200

    fix!!!

--- mods/WWIIIlauncher.js
+++ mods/WWIIIlauncher.js
@@ -1,11 +1,6 @@
 function launchWWIII() {
-    const entries = Object.entries(nations);
-    const fridx = 6;
-    const gridx = 0;
-    const [key1, value1] = entries[fridx];
-    one = { key: key1, ...value1 };
-    const [key2, value2] = entries[gridx];
-    two = { key: key2, ...value2 };
+    one = nations.GER
+    two = nations.FRA
     document.getElementById('1').textContent = one.name;
     document.getElementById('2').textContent = two.name;
 }
